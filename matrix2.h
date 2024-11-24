9a83687ddf83795604a4e9f938abef582005773c
Marcus <marcusyeww@gmail.com>
2024-11-24 13:59:06 +0800

    initial commit

---
+++ matrix2.h
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Matrix
+{
+private:
+    int M; //row index
+    int N; //column index
+    vector<vector<double>>x; 
+
+    Matrix(int M,int N){
+
+    }
+public:
+
+
+};
